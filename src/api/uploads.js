93c0994edd047dc3a053a5af8008fd912874c51a
Khawla Azhar <azharkhawlah@gmail.com>
2026-08-18 17:36:05 +0500

    configured env

--- src/api/uploads.js
+++ src/api/uploads.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/api';
+import { API_BASE_URL } from './client';
+const BASE_URL = `${API_BASE_URL}/api`;
 
 const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/webp'];
 const MAX_IMAGE_BYTES = 5 * 1024 * 1024; // 5 MB
